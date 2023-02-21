ea416dfe23c563cff0bba57cfb7df0b06a3ff730
sajaljain077 <sajal.jain@1silverbullet.tech>
2023-02-21 12:04:27 +0530

    added LRU cache implemetation from stratch

--- LRU_chache_implementation.py
+++ LRU_chache_implementation.py
@@ -1,38 +1,37 @@
-""" implementation LRU using ordered dictonary """
-# from collections import OrderedDict
+# """ implementation LRU using ordered dictonary """
+from collections import OrderedDict
 
-# class LRU:
-#     def __init__(self, capacity):
-#         self.capacity = capacity
-#         self.cache = OrderedDict()
+class LRU:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
 
-#     def get(self, key:int):
-#         if key not in self.cache:
-#             return -1
-#         else:
-#             self.cache.move_to_end(key)
-#             return self.cache[key]
+    def get(self, key:int):
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
     
-#     def put(self, key:int, value:int):
-#         self.cache[key] = value
-#         self.cache.move_to_end(key)
-#         if len(self.cache) > self.capacity:
-#             self.cache.popitem(last=False)
+    def put(self, key:int, value:int):
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
 
-# LRUObj = LRU(2)
-# param1 = LRUObj.get(1)
-# LRUObj.put(1,1)
-# LRUObj.put(2,1)
-# LRUObj.put(3,1)
-# LRUObj.put(3,1)
+# # LRUObj = LRU(2)
+# # param1 = LRUObj.get(1)
+# # LRUObj.put(1,1)
+# # LRUObj.put(2,1)
+# # LRUObj.put(3,1)
+# # LRUObj.put(3,1)
 
 
 
-""" implementation of LRU from scratch using Doubly linked list and dictonary """
-import pdb
+# """ implementation of LRU from scratch using Doubly linked list and dictonary """
+# import pdb
 
 class Node:
-
     #this is goig to be our doubly linkeed list
     #LRU to MRU
     def __init__(self, key, value):
@@ -51,21 +50,21 @@ class LRU:
     #remove from the start 
     def remove(self, node):
         print("i am inside of the remove")
-        pdb.set_trace()
+        
         prev, nxt = node.prev, node.next
         prev.next = nxt
         nxt.prev = prev
-        pdb.set_trace()
+        
 
     #we will always insert it right
     def insert(self, node):
         print("i am inside of the insert")
-        pdb.set_trace()
+        
         prev, nxt = self.right.prev, self.right
         prev.next =  nxt.prev = node
         node.next = nxt
         node.prev = prev  
-        pdb.set_trace()      
+              
 
     def get(self, key:int):
         if key in self.cache:
@@ -80,31 +79,54 @@ class LRU:
             print("i am inside of the loop where we are cheking the value inside the cache")
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
-        pdb.set_trace()
+        
         self.insert(self.cache[key])
-        pdb.set_trace()
+        
         if len(self.cache)> self.capacity:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-            pdb.set_trace()
+            
 
 
-lruObj = LRU(2)
-print(lruObj.cache)
-lruObj.get(1)
-lruObj.put(1,1)
-lruObj.put(2,2)
-pdb.set_trace()
-lruObj.get(1)
-pdb.set_trace()
-lruObj.get(2)
-pdb.set_trace()
-lruObj.put(3,3)
-pdb.set_trace()
-lruObj.put(4,4)
-pdb.set_trace()
-lruObj.get(3)
-pdb.set_trace()
-lruObj.get(4)
-pdb.set_trace()
+# lruObj = LRU(2)
+# print(lruObj.cache)
+# lruObj.get(1)
+# lruObj.put(1,1)
+# lruObj.put(2,2)
+# 
+# lruObj.get(1)
+# 
+# lruObj.get(2)
+# 
+# lruObj.put(3,3)
+# 
+# lruObj.put(4,4)
+# 
+# lruObj.get(3)
+# 
+# lruObj.get(4)
+# 
+
+
+
+from abc import ABC, abstractmethod
+
+class Student(ABC):
+
+    @abstractmethod
+    def name(self):
+        pass
+
+    @abstractmethod
+    def sirname(self):
+        pass
+
+class Sajal(Student):
+    def name(self):
+        print("this is Sajal object")
+    def sirname(self):
+        pass
+
+obj = Sajal()
+obj.name()
--- solid.py
+++ solid.py
@@ -176,89 +176,92 @@ from abc import ABC, abstractmethod
 
 
 
-# # This is after Interface segrigation, using class, subclasses
-# class Order:
-#     def __init__(self):
-#         self.items = []
-#         self.quantities = []
-#         self.prices = []
-#         self.status = "open"
+"""  This is after Interface segrigation, using class, subclasses """
+class Order:
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = "open"
 
-#     def add_item(self, name, quantity, price):
-#         self.items.append(name)
-#         self.quantities.append(quantity)
-#         self.prices.append(price)
-#     def total_price(self):
-#         total = 0
-#         for i in range(len(self.prices)):
-#             total += self.quantities[i] * self.prices[i]
-#         return total
-
-
-# class PaymentProcessor(ABC):
-#     @abstractmethod
-#     def pay(self, order):
-#         pass
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
 
 
-# class PaymentProcessor_SMS(PaymentProcessor):
-#     @abstractmethod
-#     def auth_sms(self, code):
-#         pass
+class PaymentProcessor(ABC):
+    @abstractmethod
+    def pay(self, order):
+        pass
 
 
-# class DebitPaymentProcessor(PaymentProcessor_SMS):
-#     def __init__(self, security_code):
-#         self.security_code = security_code
-#         self.verified = False
-#     def auth_sms(self, code):
-#         print(f"Verifying SMS code {code}")
-#         self.verified = True
+class PaymentProcessor_SMS(PaymentProcessor):
+    @abstractmethod
+    def auth_sms(self, code):
+        pass
+
+class DebitPaymentProcessor(PaymentProcessor_SMS):
+    def __init__(self, security_code):
+        self.security_code = security_code
+        self.verified = False
+    def auth_sms(self, code):
+        print(f"Verifying SMS code {code}")
+        self.verified = True
     
-#     def pay(self, order):
-#         if not self.verified:
-#             raise Exception("Not authorized")
-#         print("Processing debit payment type")
-#         print(f"Verifying security code: {self.security_code}")
-#         order.status = "paid"
+    def pay(self, order):
+        if not self.verified:
+            raise Exception("Not authorized")
+        print("Processing debit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
 
 
-# class CreditPaymentProcessor(PaymentProcessor):
-#     def __init__(self, security_code):
-#         self.security_code = security_code
-#     def pay(self, order):
-#         print("Processing credit payment type")
-#         print(f"Verifying security code: {self.security_code}")
-#         order.status = "paid"
+class CreditPaymentProcessor(PaymentProcessor):
+    def __init__(self, security_code):
+        self.security_code = security_code
+    def pay(self, order):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
 
 
-# class PaypalPaymentProcessor(PaymentProcessor_SMS):
-#     def __init__(self, email_address):
-#         self.email_address = email_address
-#         self.verified = False
-#     def auth_sms(self, code):
-#         print(f"Verifying SMS code {code}")
-#         self.verified = True
-#     def pay(self, order):
-#         if not self.verified:
-#             raise Exception("Not authorized")
-#         print("Processing paypal payment type")
-#         print(f"Using email address: {self.email_address}")
-#         order.status = "paid"
+class PaypalPaymentProcessor(PaymentProcessor_SMS):
+    def __init__(self, email_address):
+        self.email_address = email_address
+        self.verified = False
+    def auth_sms(self, code):
+        print(f"Verifying SMS code {code}")
+        self.verified = True
+    def pay(self, order):
+        print(self.verified)
+        if not self.verified:
+            raise Exception("Not authorized")
+        print("Processing paypal payment type")
+        print(f"Using email address: {self.email_address}")
+        order.status = "paid"
 
 
 
 
-# order = Order()
-# order.add_item("Keyboard", 1, 50)
-# order.add_item("SSD", 1, 150)
-# order.add_item("USB cable", 2, 5)
-# print(order.total_price())
-# processor = PaypalPaymentProcessor("hi@arjancodes.com")
-# processor.auth_sms(465839)
-# processor.pay(order)
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+print(order.total_price())
+processor = PaypalPaymentProcessor("hi@arjancodes.com")
+processor.auth_sms(465839)
+processor.pay(order)
 
-# This is after Interface segrigation, using composition
+
+
+
+"""     This is after Interface segrigation, using composition      """
 # class Order:
 #     def __init__(self):
 #         self.items = []
@@ -342,103 +345,103 @@ from abc import ABC, abstractmethod
 
 
 # this is after dependecy Inversion
-class Order:
-    def __init__(self):
-        self.items = []
-        self.quantities = []
-        self.prices = []
-        self.status = "open"
+# class Order:
+#     def __init__(self):
+#         self.items = []
+#         self.quantities = []
+#         self.prices = []
+#         self.status = "open"
 
-    def add_item(self, name, quantity, price):
-        self.items.append(name)
-        self.quantities.append(quantity)
-        self.prices.append(price)
-    def total_price(self):
-        total = 0
-        for i in range(len(self.prices)):
-            total += self.quantities[i] * self.prices[i]
-        return total
-
-
-class PaymentProcessor(ABC):
-    @abstractmethod
-    def pay(self, order):
-        pass
+#     def add_item(self, name, quantity, price):
+#         self.items.append(name)
+#         self.quantities.append(quantity)
+#         self.prices.append(price)
+#     def total_price(self):
+#         total = 0
+#         for i in range(len(self.prices)):
+#             total += self.quantities[i] * self.prices[i]
+#         return total
 
 
-class Authorizer(ABC):
-
-    @abstractmethod
-    def is_authorized(self)->bool:
-        return self.authorized
-
-class SMSAuth(Authorizer):
-    def __init__(self):
-        self.authorized = False
-
-    def verify_code(self, code):
-        print(f"verifying code {code}")
-        self.authorized = True
-
-    def is_authorized(self)->bool:
-        return self.authorized
+# class PaymentProcessor(ABC):
+#     @abstractmethod
+#     def pay(self, order):
+#         pass
 
 
-class RobotAuth(Authorizer):
-    def __init__(self):
-        self.authorized = False
+# class Authorizer(ABC):
 
-    def is_robot(self):
-        self.authorized = False
-        print("Are you robot.... Naaaaaa")
+#     @abstractmethod
+#     def is_authorized(self)->bool:
+#         return self.authorized
+
+# class SMSAuth(Authorizer):
+#     def __init__(self):
+#         self.authorized = False
+
+#     def verify_code(self, code):
+#         print(f"verifying code {code}")
+#         self.authorized = True
+
+#     def is_authorized(self)->bool:
+#         return self.authorized
 
 
-    def is_authorized(self)->bool:
-        return self.authorized
+# class RobotAuth(Authorizer):
+#     def __init__(self):
+#         self.authorized = False
 
-class DebitPaymentProcessor(PaymentProcessor):
-    def __init__(self, security_code, authorizer:Authorizer):
-        self.security_code = security_code
-        self.authorizer = authorizer
+#     def is_robot(self):
+#         self.authorized = False
+#         print("Are you robot.... Naaaaaa")
+
+
+#     def is_authorized(self)->bool:
+#         return self.authorized
+
+# class DebitPaymentProcessor(PaymentProcessor):
+#     def __init__(self, security_code, authorizer:Authorizer):
+#         self.security_code = security_code
+#         self.authorizer = authorizer
     
-    def pay(self, order):
-        if not self.authorizer.is_authorized():
-            raise Exception("Not authorized")
-        print("Processing debit payment type")
-        print(f"Verifying security code: {self.security_code}")
-        order.status = "paid"
+#     def pay(self, order):
+#         if not self.authorizer.is_authorized():
+#             raise Exception("Not authorized")
+#         print("Processing debit payment type")
+#         print(f"Verifying security code: {self.security_code}")
+#         order.status = "paid"
 
 
-class CreditPaymentProcessor(PaymentProcessor):
-    def __init__(self, security_code):
-        self.security_code = security_code
-    def pay(self, order):
-        print("Processing credit payment type")
-        print(f"Verifying security code: {self.security_code}")
-        order.status = "paid"
+# class CreditPaymentProcessor(PaymentProcessor):
+#     def __init__(self, security_code):
+#         self.security_code = security_code
+#     def pay(self, order):
+#         print("Processing credit payment type")
+#         print(f"Verifying security code: {self.security_code}")
+#         order.status = "paid"
 
 
-class PaypalPaymentProcessor(PaymentProcessor):
-    def __init__(self, email_address, authorizer:Authorizer):
-        self.authorizer = authorizer
-        self.email_address = email_address
+# class PaypalPaymentProcessor(PaymentProcessor):
+#     def __init__(self, email_address, authorizer:Authorizer):
+#         self.authorizer = authorizer
+#         self.email_address = email_address
 
-    def pay(self, order):
-        if not self.authorizer.is_authorized():
-            raise Exception("Not authorized")
-        print("Processing paypal payment type")
-        print(f"Using email address: {self.email_address}")
-        order.status = "paid"
-
-
+#     def pay(self, order):
+#         if not self.authorizer.is_authorized():
+#             raise Exception("Not authorized")
+#         print("Processing paypal payment type")
+#         print(f"Using email address: {self.email_address}")
+#         order.status = "paid"
 
 
-order = Order()
-order.add_item("Keyboard", 1, 50)
-order.add_item("SSD", 1, 150)
-order.add_item("USB cable", 2, 5)
-print(order.total_price())
-authorizer = RobotAuth()
-authorizer.is_robot()
-processor = PaypalPaymentProcessor("hi@arjancodes.com", authorizer)
-processor.pay(order)
+
+
+# order = Order()
+# order.add_item("Keyboard", 1, 50)
+# order.add_item("SSD", 1, 150)
+# order.add_item("USB cable", 2, 5)
+# print(order.total_price())
+# authorizer = RobotAuth()
+# authorizer.is_robot()
+# processor = PaypalPaymentProcessor("hi@arjancodes.com", authorizer)
+# processor.pay(order)
